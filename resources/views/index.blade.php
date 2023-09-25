5a749614aa523331f97880021b94af24b18ebb94
Faldian Pratama Wijaya <93422127+faldianpratama@users.noreply.github.com>
2023-09-25 17:47:21 +0700

    test2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,40 @@
 @extends('layout.frontend')
 @section('content')
-    <div style="background: yellow;">
-        <div class="text-center fs-1">
-            Nanti ini buat bodynya aja clodd!!!!!
+    <div class="container">
+        <div class="text-center py-5">
+            <h2>Anggota Berizin</h2>
+        </div>
+        <div class="row">
+
+            {{-- card  --}}
+            <div class="col-md-3 mb-5">
+                <div class="card shadow" style="max-width: 18rem;">
+                    <div class="card-header bg-transparent">
+                        <div class="row">
+                            <div class="col-6 d-flex justify-content-center">
+                                <i class="bi bi-check-circle-fill text-success me-2"></i>
+                                Berizin OJK
+                            </div>
+                            <div class="col-6 d-flex justify-content-center">
+                                <i class="bi bi-check-circle-fill text-success me-2"></i>
+                                Terverifikasi
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body text-center">
+                        <img src="{{ asset('Assets/joinprop_biru.png') }}" class="p-2 mb-3 img-fluid" width="230px" alt="logo-platform">
+                        <h5 class="card-title">Nama Platform</h5>
+                        <p class="card-text text-secondary fs-6">Nama PT</p>
+                        <p class="card-text">
+                            <a href="#" class="text-decoration-none">Kunjungi Website</a>
+                        </p>
+                    </div>
+                    <div class="card-footer bg-transparent d-flex justify-content-center">
+                        <i class="bi bi-instagram text-success me-2"></i>
+                        <a href="#" class="text-decoration-none text-dark">Akun Instagram</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light shadow shadow-sm">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="{{ asset('Assets/joinprop_biru.png') }}" class="img-fluid"
+                width="150px" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            </ul>
+            <div>
+                <button class="btn btn-outline-primary" type="submit">Login</button>
+            </div>
+        </div>
+    </div>
+</nav>
