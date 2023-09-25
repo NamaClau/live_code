6facf8ff23b8462555412853c9fda3083e564972
NamaClau <92304180+NamaClau@users.noreply.github.com>
2023-09-25 14:47:46 +0700

    test

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.frontend')
+@section('content')
+    <div style="background: yellow;">
+        <div class="text-center fs-1">
+            Nanti ini buat bodynya aja clodd!!!!!
+        </div>
+    </div>
+@endsection
